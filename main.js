a186830c408dd2b24705580f2071b1bccc233ae9
ohnitakahiro <ohnitakahiro@gmail.com>
2021-06-09 13:55:28 +0900

    comit

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("ok");
+var fruits = ['apple', 'apricot', 'avocado', 'blueberry', 'cherry', 'coconut', 'cranberry', 'dragonfruit', 'durian', 'grape', 'grapefruit', 'guava', 'kiwi fruit', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'watermelon', 'miracle fruit', 'orange', 'bloodorange','clementine','mandarine','tangerine','papaya','passionfruit','peach','pear','persimmon','physalis','plum/prune','pineapple','pomegranate','raspberry','rambutan','star fruit','strawberry'];
+
+$(function() {
+  $("#submit").on("click", function() {
+    var input = $("#keyword").val();
+    $.each(fruits, function(i, fruit) {
+      if (input === fruit) {
+        $("#result").text(input);
+        return false;
+      } 
+      else {$("#result").text ("一致する果物はありませんでした")}
+    });
+  });
+});
